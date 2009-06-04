979608827cd705c70292d04247642a3ce7b68e63
Kyle <kyle@kyle-hp.(none)>
2009-06-04 02:32:12 -0500

    added authlogic user management

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,21 @@
 class ApplicationController < ActionController::Base
   helper :all # include all helpers, all the time
 
+  helper_method :current_user
+  
+  private
+  
+  def current_user_session
+    return @current_user_session if defined?(@current_user_session)
+    @current_user_session = UserSession.find
+  end
+  
+  def current_user
+    return @current_user if defined?(@current_user)
+    @current_user = current_user_session && current_user_session.record
+  end
+  
+
   # See ActionController::RequestForgeryProtection for details
   # Uncomment the :secret if you're not using the cookie session store
   protect_from_forgery # :secret => 'f86fe8528a80847bd838645a74c2dfad'
@@ -11,5 +26,5 @@ class ApplicationController < ActionController::Base
   # See ActionController::Base for details 
   # Uncomment this to filter the contents of submitted sensitive data parameters
   # from your application log (in this case, all fields with names like "password"). 
-  # filter_parameter_logging :password
+  filter_parameter_logging :password
 end
---
+++ config/initializers/subdomains.rb
@@ -0,0 +1,3 @@
+SubdomainFu.tld_sizes = {:development => 1,
+                         :test => 0,
+                         :production => 1} # set all at once (also the defaults)
