95144f9dae5117952c270732fb22377c47d6ace9
Kyle <kyle@kyle-hp.(none)>
2009-06-12 04:30:55 -0500

    changing created_by type from string to integer

--- db/migrate/20090608074110_add_created_by_to_posts.rb
+++ db/migrate/20090608074110_add_created_by_to_posts.rb
@@ -1,6 +1,6 @@
 class AddCreatedByToPosts < ActiveRecord::Migration
   def self.up
-    add_column :posts, :created_by, :string
+    add_column :posts, :created_by, :integer
   end
 
   def self.down
