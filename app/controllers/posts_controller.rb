19c225df8927acf064e7541cc89da65fe544e5ec
Kyle <kyle@kyle-hp.(none)>
2009-06-24 02:01:03 -0500

    adding member responses to post view

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -23,6 +23,7 @@ class PostsController < ApplicationController
   # GET /posts/1.xml
   def show
     @post = Post.find(params[:id])
+    @members = Member.all
 
     respond_to do |format|
       format.html # show.html.erb
