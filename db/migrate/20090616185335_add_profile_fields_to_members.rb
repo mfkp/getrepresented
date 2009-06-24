5679142af535d70f78f805203cffbd4a94361874
Kyle <kyle@kyle-hp.(none)>
2009-06-24 01:15:52 -0500

    creating members table

--- db/migrate/20090616185335_add_profile_fields_to_members.rb
+++ db/migrate/20090616185335_add_profile_fields_to_members.rb
@@ -1,11 +1,20 @@
 class AddProfileFieldsToMembers < ActiveRecord::Migration
   def self.up
+    create_table :members do |t|
+      t.string :username
+      t.string :email
+      t.string :crypted_password
+      t.string :password_salt
+      t.string :persistence_token
+      t.timestamps
+    end
     add_column "members", "first_name", :string
     add_column "members", "last_name", :string
     add_column "members", "profile", :text
   end
 
   def self.down
+    drop_table :members
     remove_column "members", "first_name"
     remove_column "members", "last_name"
     remove_column "members", "profile"
