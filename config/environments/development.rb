dbae04f66ddeb405237c531cab4f9ed93ccf71fd
Kyle <kyle@kyle-hp.(none)>
2009-07-08 15:52:06 -0500

    changing session domain

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -16,4 +16,4 @@ config.action_controller.perform_caching             = false
 # Don't care if the mailer can't send
 config.action_mailer.raise_delivery_errors = false
 
-ActionController::Base.session_options[:session_domain] = 'myagenda.local'
+ActionController::Base.session_options[:session_domain] = 'myagenda.org'
