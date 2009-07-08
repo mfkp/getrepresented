1fc53582ef8a9e0a31c4d6f77ba3bd5b0ef1a5a0
Kyle <kyle@kyle-hp.(none)>
2009-07-08 15:54:50 -0500

    modifying session domain

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -16,4 +16,5 @@ config.action_controller.perform_caching             = false
 # Don't care if the mailer can't send
 config.action_mailer.raise_delivery_errors = false
 
-ActionController::Base.session_options[:session_domain] = 'myagenda.org'
+ActionController::Base.session_options[:session_domain] = '.myagenda.org'
+ActionController::CgiRequest::DEFAULT_SESSION_OPTIONS.update( :session_domain => '.myagenda.org' )
