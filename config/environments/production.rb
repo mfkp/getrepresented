8706f668f45c761b8c148ce9bb3ff547c3ed1761
Kyle <kyle@kyle-hp.(none)>
2009-07-08 16:26:20 -0500

    testing subdomain options

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -21,6 +21,6 @@ config.action_view.cache_template_loading            = true
 # Disable delivery errors, bad email addresses will be ignored
 # config.action_mailer.raise_delivery_errors = false
 
-ActionController::Base.session_options[:domain] = "myagenda.org"
+ActionController::Base.session_options[:domain] = ".myagenda.org"
 #ApplicationController.session_options.update( :session_domain => 'myagenda.org' )
 #ActionController::CgiRequest::DEFAULT_SESSION_OPTIONS.update( :session_domain => '.myagenda.org' )
