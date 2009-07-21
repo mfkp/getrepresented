20c59692a29cb4594d06ac8e0796d0901657b13e
PSIWeb <psiweb@psiwebs-macbook-pro.local>
2009-07-20 21:01:38 -0500

    removing acts_as_taggable from post model

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,7 +2,7 @@ class Post < ActiveRecord::Base
   #acts_as_authorization_object
   #acts_as_authorizable
   acts_as_voteable
-  acts_as_taggable_on :tags
+  #acts_as_taggable_on :tags
   
   logical_parent :member
   belongs_to :user, :foreign_key => 'created_by', :class_name => 'User'
