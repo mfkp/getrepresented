4b4d3b135b9044b691142af5cf77b53211aa03ae
PSIWeb <psiweb@psiwebs-macbook-pro.local>
2009-07-25 02:20:32 -0500

    changing member migration structure

--- db/migrate/20090616185333_create_members.rb
+++ db/migrate/20090616185333_create_members.rb
@@ -3,6 +3,9 @@ class CreateMembers < ActiveRecord::Migration
     create_table :members do |t|
       t.string :username
       t.string :email
+      t.string :first_name
+      t.string :last_name
+      t.text :profile
       t.string :crypted_password
       t.string :password_salt
       t.string :persistence_token
