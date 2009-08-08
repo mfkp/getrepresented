dd9a16d940b2e7c7b7924f3bc1a4761ff4b21c2f
kjulius <kylepowers@gmail.com>
2009-08-08 02:43:06 -0500

    adding openid to user registration and login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,34 +5,36 @@ class UsersController < ApplicationController
   
   def create
     @user = User.new(params[:user])
-      geo = Geokit::Geocoders::GoogleGeocoder.geocode @user.street_address + ", " + @user.city + ", " + @user.state + " " + @user.zip
-    @user.lat = geo.lat
-    @user.lng = geo.lng
-    if @user.save
-       @mymembers = Sunlight::Legislator.all_for(:latitude => @user.lat, :longitude => @user.lng)
-         @senior_senator = @mymembers[:senior_senator]
-         @junior_senator = @mymembers[:junior_senator]
-         @representative = @mymembers[:representative]
-         
-         if !@senior_senator.nil?
-            @ss_id =Member.find_by_sql(["select id from members where first_name=? and last_name=?", @senior_senator.firstname, @senior_senator.lastname])
-            @membership = current_user.memberships.build(:member_id => @ss_id[0].id)
-            @membership.save
-         end
-         if !@junior_senator.nil?
-            @js_id =Member.find_by_sql(["select id from members where first_name=? and last_name=?", @junior_senator.firstname, @junior_senator.lastname])
-            @membership = current_user.memberships.build(:member_id => @js_id[0].id)
-            @membership.save
-         end
-         if !@representative.nil?
-            @rep_id =Member.find_by_sql(["select id from members where first_name=? and last_name=?", @representative.firstname, @representative.lastname])
-            @membership = current_user.memberships.build(:member_id => @rep_id[0].id)
-            @membership.save
-         end
-      flash[:notice] = "Registration Successful."
-      redirect_to root_url
-    else
-      render :action => 'new'
+    @user.save do |result|
+      if result
+        geo = Geokit::Geocoders::GoogleGeocoder.geocode @user.street_address + ", " + @user.city + ", " + @user.state + " " + @user.zip
+        @user.lat = geo.lat
+        @user.lng = geo.lng
+         @mymembers = Sunlight::Legislator.all_for(:latitude => @user.lat, :longitude => @user.lng)
+           @senior_senator = @mymembers[:senior_senator]
+           @junior_senator = @mymembers[:junior_senator]
+           @representative = @mymembers[:representative]
+           
+           if !@senior_senator.nil?
+              @ss_id =Member.find_by_sql(["select id from members where first_name=? and last_name=?", @senior_senator.firstname, @senior_senator.lastname])
+              @membership = current_user.memberships.build(:member_id => @ss_id[0].id)
+              @membership.save
+           end
+           if !@junior_senator.nil?
+              @js_id =Member.find_by_sql(["select id from members where first_name=? and last_name=?", @junior_senator.firstname, @junior_senator.lastname])
+              @membership = current_user.memberships.build(:member_id => @js_id[0].id)
+              @membership.save
+           end
+           if !@representative.nil?
+              @rep_id =Member.find_by_sql(["select id from members where first_name=? and last_name=?", @representative.firstname, @representative.lastname])
+              @membership = current_user.memberships.build(:member_id => @rep_id[0].id)
+              @membership.save
+           end
+        flash[:notice] = "Registration Successful."
+        redirect_to root_url
+      else
+        render :action => 'new'
+      end
     end
   end
   
@@ -42,11 +44,14 @@ class UsersController < ApplicationController
   
   def update
     @user = current_user
-    if @user.update_attributes(params[:user])
-      flash[:notice] = "Successfully updated profile."
-      redirect_to root_url
-    else
-      render :action => 'edit'
+    @user.attributes = params[:user]
+    @user.save do |result|
+      if result
+        flash[:notice] = "Successfully updated profile."
+        redirect_to root_url
+      else
+        render :action => 'edit'
+      end
     end
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,30 @@
 class User < ActiveRecord::Base
-  acts_as_authentic
+  acts_as_authentic do |c|
+    c.openid_required_fields = [:nickname, :email, :postcode]
+  end
+
   acts_as_voter
-  #acts_as_authorization_subject
-  #acts_as_authorized_user
   
-  #has_and_belongs_to_many :members
   has_many :memberships
   has_many :members, :through => :memberships
+  
+  validates_uniqueness_of :username
+  validates_presence_of :password, :unless => :openid_identifier
+  validates_size_of :password, :within => 5..100, :unless => :openid_identifier
+  validates_format_of :email,
+    :with => /^([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})$/i
+  validates_format_of :username, :with => /^\w+$/i,
+    :message => "can only contain letters and numbers."
+  validates_confirmation_of :password
+  validates_presence_of :street_address, :city, :state, :zip
 
+  
+  private
+  
+  def map_openid_registration(registration)
+    self.email = registration["email"] if email.blank?
+    self.username = registration["nickname"] if username.blank?
+    self.zip = registration["postcode"] if zip.blank?
+  end
+  
 end
