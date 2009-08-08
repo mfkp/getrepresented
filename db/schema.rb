363070e409b0a07ee996a75f0c101cf3224f45b3
kjulius <kylepowers@gmail.com>
2009-08-07 22:04:25 -0500

    updating schema

--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20090725061719) do
+ActiveRecord::Schema.define(:version => 20090725061720) do
 
   create_table "categories", :force => true do |t|
     t.string   "name"
@@ -27,14 +27,14 @@ ActiveRecord::Schema.define(:version => 20090725061719) do
   create_table "members", :force => true do |t|
     t.string   "username"
     t.string   "email"
+    t.string   "first_name"
+    t.string   "last_name"
+    t.text     "profile"
     t.string   "crypted_password"
     t.string   "password_salt"
     t.string   "persistence_token"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.string   "first_name"
-    t.string   "last_name"
-    t.text     "profile"
     t.string   "title"
     t.string   "party"
     t.string   "state"
@@ -93,19 +93,6 @@ ActiveRecord::Schema.define(:version => 20090725061719) do
     t.datetime "created_at"
     t.datetime "updated_at"
   end
-
-  create_table "taggings", :force => true do |t|
-    t.integer  "tag_id"
-    t.integer  "taggable_id"
-    t.integer  "tagger_id"
-    t.string   "tagger_type"
-    t.string   "taggable_type"
-    t.string   "context"
-    t.datetime "created_at"
-  end
-
-  add_index "taggings", ["tag_id"], :name => "index_taggings_on_tag_id"
-  add_index "taggings", ["taggable_id", "taggable_type", "context"], :name => "index_taggings_on_taggable_id_and_taggable_type_and_context"
 
   create_table "tags", :force => true do |t|
     t.string "name"
