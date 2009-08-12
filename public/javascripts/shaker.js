aec33912f1f8b3b66ec02830bee75fd39325f1ed
kjulius <kylepowers@gmail.com>
2009-08-12 17:02:09 -0500

    fixing another javascript error in IE

--- public/javascripts/shaker.js
+++ public/javascripts/shaker.js
@@ -1,21 +1,29 @@
 /* shake baby! */
 window.addEvent('domready',function() {
-	var shaker = $('shaker');
-	shaker.setStyles({
-		opacity: 0,
-		display: 'block'
-	});
-	/* event */
-	window.addEvent('load',function() {
-		/* fade in */
-		var x = function() { shaker.fade('in');};
-		x.delay(1000);
-		/* shake */
-		var y = function() { shaker.shake('margin',5,3); };
-		y.delay(3000); /* shake once faded in */
-		y.periodical(15000); /* then shake every 15 seconds */
-		/* fade out */
-		var z = function() { shaker.fade('out');};
-		z.delay(35000); /* fade out after 35 seconds */
-	});
+	if (document.getElementById('shaker')) {
+		var shaker = $('shaker');
+		shaker.setStyles({
+			opacity: 0,
+			display: 'block'
+		});
+		/* event */
+		window.addEvent('load', function(){
+			/* fade in */
+			var x = function(){
+				shaker.fade('in');
+			};
+			x.delay(1000);
+			/* shake */
+			var y = function(){
+				shaker.shake('margin', 5, 3);
+			};
+			y.delay(3000); /* shake once faded in */
+			y.periodical(15000); /* then shake every 15 seconds */
+			/* fade out */
+			var z = function(){
+				shaker.fade('out');
+			};
+			z.delay(35000); /* fade out after 35 seconds */
+		});
+	};
 });
