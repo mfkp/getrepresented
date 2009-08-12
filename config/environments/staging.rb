85cde55894ae03ca96d386ac876f6ddb98a8c762
kjulius <kylepowers@gmail.com>
2009-08-11 22:09:44 -0500

    changing domain for cookies

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -21,4 +21,4 @@ config.action_view.cache_template_loading            = true
 # Disable delivery errors, bad email addresses will be ignored
 # config.action_mailer.raise_delivery_errors = false
 
-config.action_controller.session[:domain] = '.getrep.sunlightlabs.com'
+config.action_controller.session[:domain] = '.getrep.sunlightlabs.net'
